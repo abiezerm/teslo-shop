46bc4c09f06fb2a80ca9fdac322dd3589de3a329
Abiezer Matos <abiezerm@gmail.com>
2024-05-21 22:01:33 -0400

    missing fileFilter.helper

---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,13 @@
+export const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  callback: Function,
+) => {
+  if (!file) return callback(new Error('File is empty'), false);
+  const fileExtension = file.mimetype.split('/')[1];
+  const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+  if (!allowedExtensions.includes(fileExtension)) return callback(null, false);
+
+  callback(null, true);
+};
